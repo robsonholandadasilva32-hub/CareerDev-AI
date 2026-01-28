e31974c7baff3c4f0b02ac0fd1162ee27bb736b3
ROBSON HOLANDA DA SILVA <robsonholandadasilva32@gmail.com>
2026-01-28 20:49:47 -0300

    Update audit.py
    Nova Atualização!

--- app/db/models/audit.py
+++ app/db/models/audit.py
@@ -3,26 +3,25 @@ from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.db.base_class import Base
 
-# --- CORREÇÃO FINAL: O nome da classe DEVE ser LoginHistory ---
-# Se estiver como "class AuditLog(Base):", o sistema QUEBRA.
+# --- ATENÇÃO: O nome da classe deve ser LoginHistory ---
 class LoginHistory(Base):
     __tablename__ = "login_history"
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
-    session_id = Column(String, nullable=True) 
+    session_id = Column(String, nullable=True)
 
-    # Dados Forenses
+    # Forensic Data
     ip_address = Column(String, nullable=True)
     user_agent_raw = Column(String, nullable=True)
     device_type = Column(String, nullable=True)
     browser = Column(String, nullable=True)
     os = Column(String, nullable=True)
 
-    # Metadados da Sessão
+    # Session Metadata
     login_timestamp = Column(DateTime, default=datetime.utcnow)
     is_active_session = Column(Boolean, default=True)
     auth_method = Column(String, nullable=True)
 
-    # Relacionamento Reverso
+    # Relacionamento
     user = relationship("User", back_populates="audit_logs")
