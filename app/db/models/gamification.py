b0fd8413d653dc38ac64e07b3cc3a54096fec693
ROBSON HOLANDA DA SILVA <robsonholandadasilva32@gmail.com>
2026-01-28 19:22:47 -0300

    Update gamification.py
    Atualização!

--- app/db/models/gamification.py
+++ app/db/models/gamification.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from app.db.declarative import Base
+from app.db.base_class import Base
 
 class Badge(Base):
     __tablename__ = "badges"
