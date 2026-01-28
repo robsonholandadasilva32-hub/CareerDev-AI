9b2f381841d406d11a4146343e9b457502a0cabb
ROBSON HOLANDA DA SILVA <robsonholandadasilva32@gmail.com>
2026-01-28 19:12:06 -0300

    Update career.py
    Atualização!

--- app/db/models/career.py
+++ app/db/models/career.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, JSON, DateTime, Text
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from app.db.declarative import Base
+from app.db.base_class import Base
 
 class CareerProfile(Base):
     __tablename__ = "career_profiles"
