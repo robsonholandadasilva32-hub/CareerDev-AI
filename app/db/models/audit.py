c2d1c94271313b9e8561a3419af98f4a3d81891f
ROBSON HOLANDA DA SILVA <robsonholandadasilva32@gmail.com>
2026-01-29 07:04:01 -0300

    Update audit.py
    Atual

--- app/db/models/audit.py
+++ app/db/models/audit.py
@@ -5,24 +5,25 @@ from app.db.base_class import Base
 
 class AuditLog(Base):
     __tablename__ = "audit_logs"
-    # ADICIONE ESTA LINHA PARA RESOLVER O ERRO "ALREADY DEFINED":
+    # Mantemos isso por segurança caso a tabela já exista na memória
     __table_args__ = {'extend_existing': True}
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     session_id = Column(String, nullable=True)
 
-    # Forensic Data
+    # Dados Forenses
     ip_address = Column(String, nullable=True)
     user_agent_raw = Column(String, nullable=True)
     device_type = Column(String, nullable=True)
     browser = Column(String, nullable=True)
     os = Column(String, nullable=True)
 
-    # Session Metadata
+    # Metadados
     login_timestamp = Column(DateTime, default=datetime.utcnow)
     is_active_session = Column(Boolean, default=True)
     auth_method = Column(String, nullable=True)
 
-    # Relacionamento Reverso
-    user = relationship("User", back_populates="audit_logs")
+    # --- CORREÇÃO BLINDADA AQUI ---
+    # Caminho completo para evitar erro de "Multiple classes found"
+    user = relationship("app.db.models.user.User", back_populates="audit_logs")
